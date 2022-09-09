adfad74053a88b9d629f1e02164699d6365688a6
JulianLangberg <JulianLangberg@gmail.com>
2022-09-09 10:53:07 -0300

    commit

--- streamlit/conection_databricks.py
+++ streamlit/conection_databricks.py
@@ -2,7 +2,7 @@ from databricks import sql
 import pandas as pd
 
 def tabla(db,tabla):
-    connection =  sql.connect(server_hostname = 'https://dbc-bd9b117a-06cc.cloud.databricks.com/',
+    connection =  sql.connect(server_hostname = 'https://dbc-bd9b117a-06cc.cloud.databricks.com',
                               http_path       = 'sql/protocolv1/o/4317428421786124/0908-202625-7ocde901',
                               access_token    = 'dapi025ca0bebe2941d57eee4ccbd170cace')
 
